b060a7d157c0e76dba289b2f3cceb7993e9f49f5
mo36924 <mo36924@users.noreply.github.com>
2021-01-28 13:57:53 +0900

    🎉

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # changestate
+
 changestate
---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "commonjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.mjs",
+        format: "module",
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "module" }],
+    plugins: [dts()],
+  },
+];
